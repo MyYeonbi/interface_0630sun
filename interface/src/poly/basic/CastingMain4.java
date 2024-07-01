29a13864ffe81cc4ac75ad244250b671a6fd8f00
MyYeonbi <yeonbi_kim@icloud.com>
2024-07-01 17:38:45 +0900

    Initial commit

---
+++ interface/src/poly/basic/CastingMain4.java
@@ -0,0 +1,20 @@
+package poly.basic;
+
+//다운캐스팅을 자동으로 하지 않는 이유!
+public class CastingMain4 {
+    public static void main(String[] args) {
+        Parent parent1 = new Child();
+        //Child child1 = parent1;
+        // child에 parent타입 못 넣음.
+        // 아이가 엄마의 집과 차를 관리 못함.
+
+        Child child1 = (Child) parent1;
+        child1.childMethod();
+
+        Parent parent2 = new Parent();
+        Child child2 = (Child) parent2;
+        //런타임 오류 - ClassCastException
+        child2.parentMethod();//실행 불가
+
+    }
+}
